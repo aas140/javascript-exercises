da737c1dcdf8d8f49b322322cfa5a43d85c84bd4
aastha <280506420+aas140@users.noreply.github.com>
2026-05-16 18:03:45 +0530

    Complete 09-sumAll exercise
    function returns the sum of integers in range[a,b]

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,13 @@
-const sumAll = function() {
-
+const sumAll = function(a, b) {
+    if(Number.isInteger(a) && Number.isInteger(b) && a>0 && b>0){
+        let l = Math.min(a,b);
+        let h = Math.max(a,b);
+        let sum=0;
+        for(let i=l; i<=h; i++) sum+=i;
+        return sum;
+    }
+    else return "ERROR";
+    
 };
 
 // Do not edit below this line
